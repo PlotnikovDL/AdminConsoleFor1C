7807e0f40144a861e46dd60804824c88bb18dec2
Dmitrii Plotnikov <plotnikov1c@ya.ru>
2026-05-31 07:06:02 +0300

    Добавил приложение WinUI 3

---
+++ src/AdminConsoleFor1C.App/App.xaml.cs
@@ -0,0 +1,29 @@
+using Microsoft.UI.Xaml;
+
+namespace AdminConsoleFor1C.App;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+public partial class App : Application
+{
+    private Window? _window;
+
+    /// <summary>
+    /// Initializes the singleton application object.
+    /// </summary>
+    public App()
+    {
+        InitializeComponent();
+    }
+
+    /// <summary>
+    /// Invoked when the application is launched.
+    /// </summary>
+    /// <param name="args">Details about the launch request and process.</param>
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _window = new MainWindow();
+        _window.Activate();
+    }
+}
